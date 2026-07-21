544bf61d6f8a9dadeeb22ae29123f466bfdf1671
Faith Oluwasefunmi <faithorilabawaye@gmail.com>
2026-07-21 05:47:32 +0100

    Fix component import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useRef } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
-import Food from "./components/food";
+import Food from "./components/Food";
 import Category from "./components/Category";
 import Delivery from "./components/Delivery";
 import Pickup from "./components/Pickup";
